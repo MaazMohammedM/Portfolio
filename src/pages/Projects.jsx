36dd95967749d9e8f72e19ec572d2f7e8a62b165
MaazMohammedM <maazmohammed742@gmail.com>
2024-02-05 18:54:48 +0530

    Project Links Update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,18 +11,9 @@ const Projects =()=>{
             <h2 className=" text-secondary-100 text-sm  sm:text-[1.5rem] font-bold font-accent leading-10 mb-6 sm:mb-12">&#60;Projects I Have Done&#62;</h2>
             <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4 sm:gap-6 justify-items-center  mx-auto w-4/5 xl:w-3/4 mb-12">
                 {projectData.map((i)=>{
-                    return <ProjectCard key={i.id} backgroundImage={i.image} heading={i.heading} para={i.paragraph}/>
+                    return <ProjectCard key={i.id} backgroundImage={i.image} heading={i.heading} para={i.paragraph} link={i.link}/>
                 })}
             </div>
-            {/* <a className={clsx('bg-accent-100 font-primary px-2 py-2 text-neutral-900 text-xs sm:text-base transition-all font-medium leading-7 self-center rounded-lg',
-                            //hover
-                            'hover:bg-accent-200',
-                            //focus
-                            'outline-none',
-                            'ring-accent-200/70 ring-offset-2',
-                            'focus-visible:ring-2 focus:scale-[0.98]',
-                            )}
-            type="button" href="http://www.google.com">View All Projects &#62;</a> */}
                 </div>
         </section>
     )
