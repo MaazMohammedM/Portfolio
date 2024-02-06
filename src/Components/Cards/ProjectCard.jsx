7675da38689fecedb4805a97cd30df44c3c948c5
MaazMohammedM <maazmohammed742@gmail.com>
2024-02-06 09:45:32 +0530

    Link Updated

--- src/Components/Cards/ProjectCard.jsx
+++ src/Components/Cards/ProjectCard.jsx
@@ -20,7 +20,7 @@ const ProjectCard =({backgroundImage,heading,para,link})=>{
             <div className={`w-full h-40 bg-center bg-no-repeat bg-cover rounded-tl-xl rounded-tr-xl mb-2`} style={cardBg}></div>
             <h5 className="px-2 mb-2 text-secondary-900 text-sm sm:text-base font-bold w-full font-accent leading-normal">{TruncateHeading}</h5>
             <p className="text-secondary-900 mb-4 w-full text-xs sm:text-sm font-normal font-primary leading-normal px-2">{TruncatePara}</p>
-            <a type="button" className="text-accent-700 mx-2 text-xs sm:text-sm mb-2 w-[100%-0.5rem] text-center rounded-lg py-1 block hover:text-accent-400" href={link}>View Project &#62;</a>
+            <a type="button" className="text-accent-700 mx-2 text-xs sm:text-sm mb-2 w-[100%-0.5rem] text-center rounded-lg py-1 block hover:text-accent-400"  target="_blank" href={link}>View Project &#62;</a>
         </section>
     )
 }
