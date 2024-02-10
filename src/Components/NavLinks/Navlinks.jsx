f3ac6c8034562070f7f79f7fe5973fdcdb61c57d
MaazMohammedM <maazmohammed742@gmail.com>
2024-02-10 22:00:30 +0530

    updated few changes in link

--- src/Components/NavLinks/Navlinks.jsx
+++ src/Components/NavLinks/Navlinks.jsx
@@ -5,7 +5,7 @@ export default function Navlinks({className,classNameLink}){
     return(
         <nav className={clsx(`${className}`)}>
             <ul className={clsx(`flex justify-between items-center font-primary ${classNameLink}`)}>
-                <li><a className="sm:py-4 sm:px-6 text-sm px-0 py-4 block text-neutral-50 hover:text-neutral-400 transition-all duration-150" href="index.html">Home</a></li>
+                <li><a className="sm:py-4 sm:px-6 text-sm px-0 py-4 block text-neutral-50 hover:text-neutral-400 transition-all duration-150" href="/">Home</a></li>
                 <li><a className="sm:py-4 sm:px-6 text-sm px-0 py-4 block text-neutral-50 hover:text-neutral-400 transition-all duration-150" href="#about">About</a></li>
                 <li><a className="sm:py-4 sm:px-6 text-sm px-0 py-4 block text-neutral-50 hover:text-neutral-400 transition-all duration-150" href="#projects">Projects</a></li>
                 <li><a className="sm:py-4 sm:px-6 text-sm px-0 py-4 block text-accent-100 sm:text-dark-100 sm:bg-accent-100 sm:hover:bg-accent-800 sm:hover:text-neutral-100 transition-all duration-150" href="#contact">Contact</a></li>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,7 @@ import projectData from '../Data/ProjectDataJson.json';
 const Projects =()=>{
 
     return(
-        <section className="w-full bg-dark-100 px-2 sm:px-16 py-12 sm:py-[5rem]  text-neutral-50 h-full">
+        <section id="projects" className="w-full bg-dark-100 px-2 sm:px-16 py-12 sm:py-[5rem]  text-neutral-50 h-full">
             <div className="container max-w-[90rem] flex flex-col mx-auto">
             <h2 className=" text-secondary-100 text-sm  sm:text-[1.5rem] font-bold font-accent leading-10 mb-6 sm:mb-12">&#60;Projects I Have Done&#62;</h2>
             <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4 sm:gap-6 justify-items-center  mx-auto w-4/5 xl:w-3/4 mb-12">
