96bba649f840f0dea838c0800afa5d2ec09f138c
MaazMohammedM <maazmohammed742@gmail.com>
2024-02-15 00:07:20 +0530

    updated Button

--- src/Components/Buttons/Buttons.jsx
+++ src/Components/Buttons/Buttons.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Button =({children,className,style,link,onclick})=>{
+const Button =({children,className,style,link,onClick})=>{
     return(
-        <button onClick={onclick} className={` font-primary text-neutral-900 text-xs md:text-base transition-all font-medium leading-7 hover:cursor-pointer rounded-lg ${className}`} 
+        <button  className={` font-primary text-neutral-900 text-xs md:text-base transition-all font-medium leading-7 hover:cursor-pointer rounded-lg ${className}`} 
         style={{
             padding: '0.5em 1em', // Adjust padding ratio as needed
             fontSize: style?.fontSize || '1rem',
             ...style,
           }}>
-            <a className="outline-none text-xs md:text-base text-inherit" href={link}>{children}</a>
+            <a onClick={onClick} className="outline-none text-xs md:text-base text-inherit" href={link}>{children}</a>
         </button>
     )
 }
